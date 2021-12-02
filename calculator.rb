a30cd3a39c2713a5ff30dce609749e5c7f6d79ea
Isabella <apple@Isabella.local>
2021-12-02 11:44:29 +0000

    add first commit

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+#add numbers , subtract numbers, divide numbers, multiply numbers
+
+class Calculator
+  def subtract(a,b)
+   puts "The answer is #{a - b}"
+  end
+
+  def multiply(a,b)
+   puts "The answer is #{a * b}"
+ end
+
+  def add(a,b)
+    puts "the answer is #{a + b}"
+  end
+
+  def divide(a,b)
+    puts "the answer is #{a / b}"
+  end
+end
